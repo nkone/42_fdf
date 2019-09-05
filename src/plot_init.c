4bf550fdc4df4b05bb5968929cd740f614c3f9b8
nkone <phtruong@protonmail.com>
2019-09-04 23:02:09 -0700

    removing first and second point

--- src/plot_init.c
+++ src/plot_init.c
@@ -6,7 +6,7 @@
 /*   By: phtruong <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 16:30:57 by phtruong          #+#    #+#             */
-/*   Updated: 2019/08/30 13:05:01 by phtruong         ###   ########.fr       */
+/*   Updated: 2019/09/04 22:58:18 by phtruong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,47 +38,65 @@ void	plot_line_init(t_pt *p0, t_pt *p1, t_var *var)
 
 void	plot_line_first_pt(t_fdf *fdf, t_var *var, t_pt p0)
 {
+	double p;
+
+	p = 0.0;
 	var->xend = round(p0.x);
 	var->yend = p0.y + var->gradient * (var->xend - p0.x);
 	var->xgap = rfpart(p0.x + 0.5);
 	var->xpxl1 = var->xend;
 	var->ypxl1 = ipart(var->yend);
+/*	
 	if (var->steep)
 	{
+		p = (var->swap_d) ? 1.0 : 0.0;
 		plot_pixel(fdf, var->ypxl1, var->xpxl1,
-				get_color(p0.rgb, p0.rgb, 1.0, rfpart(var->yend) * var->xgap));
+				get_color(p0.rgb, p0.rgb, p, rfpart(var->yend) * var->xgap));
 		plot_pixel(fdf, var->ypxl1 + 1, var->xpxl1,
-				get_color(p0.rgb, p0.rgb, 1.0, fpart(var->yend) * var->xgap));
+				get_color(p0.rgb, p0.rgb, p, fpart(var->yend) * var->xgap));
 	}
 	else
 	{
+		p = (var->swap) ? 1.0 : 0.0;
 		plot_pixel(fdf, var->xpxl1, var->ypxl1,
-				get_color(p0.rgb, p0.rgb, 1.0, rfpart(var->yend) * var->xgap));
+				get_color(p0.rgb, p0.rgb, p, rfpart(var->yend) * var->xgap));
 		plot_pixel(fdf, var->xpxl1 + 1, var->ypxl1,
-				get_color(p0.rgb, p0.rgb, 1.0, fpart(var->yend) * var->xgap));
+				get_color(p0.rgb, p0.rgb, p, fpart(var->yend) * var->xgap));
 	}
+*/
+
+	(void)fdf;
 }
 
 void	plot_line_second_pt(t_fdf *fdf, t_var *var, t_pt p1)
 {
+	double p;
+
+	p = 0.0;
+
 	var->intery = var->yend + var->gradient;
 	var->xend = round(p1.x);
 	var->yend = p1.y + var->gradient * (var->xend - p1.x);
 	var->xgap = fpart(p1.x + 0.5);
 	var->xpxl2 = var->xend;
 	var->ypxl2 = ipart(var->yend);
+/*
 	if (var->steep)
 	{
+		p = (var->swap_d) ? 1.0 : 1.0;
 		plot_pixel(fdf, var->ypxl2, var->xpxl2,
-				get_color(p1.rgb, p1.rgb, 1.0, rfpart(var->yend) * var->xgap));
+				get_color(p1.rgb, p1.rgb, p, rfpart(var->yend) * var->xgap));
 		plot_pixel(fdf, var->ypxl2 + 1, var->xpxl2,
-				get_color(p1.rgb, p1.rgb, 1.0, fpart(var->yend) * var->xgap));
+				get_color(p1.rgb, p1.rgb, p, fpart(var->yend) * var->xgap));
 	}
 	else
 	{
+		p = (var->swap) ? 1.0 : 1.0;
 		plot_pixel(fdf, var->xpxl2, var->ypxl2,
-				get_color(p1.rgb, p1.rgb, 1.0, rfpart(var->yend) * var->xgap));
+				get_color(p1.rgb, p1.rgb, p, rfpart(var->yend) * var->xgap));
 		plot_pixel(fdf, var->xpxl2, var->ypxl2 + 1,
-				get_color(p1.rgb, p1.rgb, 1.0, fpart(var->yend) * var->xgap));
+				get_color(p1.rgb, p1.rgb, p, fpart(var->yend) * var->xgap));
 	}
+*/
+	(void)fdf;
 }
